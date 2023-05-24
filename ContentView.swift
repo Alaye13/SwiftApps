2e108b2b1f87e1733cc91ba79fc7779b06bdbaec
Ifenna Ekwenem <101848840+Alaye13@users.noreply.github.com>
2023-05-23 22:16:24 -0400

    Add files via upload

--- ContentView.swift
+++ ContentView.swift
@@ -1,62 +1,120 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var coinResult = "Heads"
-    @State private var flipsHistory: [String] = []
+    
+    @State private var board = [0,0,0,0,0,0,0,0,0]
+    @State private var player1Turn = true
+    @State private var message = "Player 1's Turn"
+    
+    let winningCombinations = [
+        [0,1,2],[3,4,5],[6,7,8],
+        [0,3,6],[1,4,7],[2,5,8],
+        [0,4,8],[2,4,6]
+    ]
     
     var body: some View {
         VStack {
-            Text(coinResult)
-                .font(.system(size: 50))
-                .foregroundColor(coinResult == "Heads" ? .green : .red)
-                .padding()
+            Text(message)
+                .font(.headline)
+                .padding(.bottom, 30)
             
-            Button(action: {
-                self.flipCoin()
-            }) {
-                Image(systemName: coinResult == "Heads" ? "circle.fill" : "circle")
-                    .resizable()
-                    .frame(width: 25, height: 25)
-                    .foregroundColor(.white)
-                
-                Text("Flip")
-                    .font(.title)
-                    .foregroundColor(.white)
-                    .padding(.leading, 10)
+            LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 3), spacing: 5) {
+                ForEach(0..<9) { index in
+                    Button(action: {
+                        boardButtonPressed(index)
+                    }) {
+                        Text(getButtonTitle(index))
+                            .font(.system(size: getButtonFontSize(getButtonTitle(index))))
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            .background(Color.gray.opacity(0.3))
+                            .cornerRadius(10)
+                    }
+                    .disabled(board[index] != 0)
+                }
             }
             .padding()
-            .background(Color.blue)
-            .cornerRadius(10)
             
-            Divider()
-            
-            VStack(alignment: .leading) {
-                Text("Flips History:")
-                    .font(.headline)
-                
-                ForEach(flipsHistory.reversed(), id: \.self) { flip in
-                    Text(flip)
-                }
+            Button(action: {
+                resetButtonPressed()
+            }) {
+                Text("Reset")
+                    .font(.title2)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
             }
             .padding()
         }
     }
     
-    func flipCoin() {
-        let randomResult = Int.random(in: 0...1)
-        coinResult = randomResult == 0 ? "Heads" : "Tails"
-        
-        if flipsHistory.count >= 10 {
-            flipsHistory = Array(flipsHistory.suffix(9))
+    private func boardButtonPressed(_ index: Int) {
+        if player1Turn {
+            board[index] = 1
+            message = "Player 2's Turn"
+        } else {
+            board[index] = 2
+            message = "Player 1's Turn"
         }
         
-        flipsHistory.append(coinResult)
+        if checkForWin() {
+            message = player1Turn ? "Player 1 Wins!" : "Player 2 Wins!"
+            disableBoard()
+        } else if checkForTie() {
+            message = "It's a tie!"
+            disableBoard()
+        } else {
+            player1Turn.toggle()
+        }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    private func resetButtonPressed() {
+        board = [0,0,0,0,0,0,0,0,0]
+        player1Turn = true
+        message = "Player 1's Turn"
     }
+    
+    private func getButtonTitle(_ index: Int) -> String {
+        switch board[index] {
+        case 1:
+            return "X"
+        case 2:
+            return "O"
+        default:
+            return " "
+        }
+    }
+    
+    private func getButtonFontSize(_ title: String) -> CGFloat {
+        switch title {
+        case "X", "O":
+            return 120
+        default:
+            return 120
+        }
+    }
+    
+    private func checkForWin() -> Bool {
+        for combination in winningCombinations {
+            if board[combination[0]] != 0 && board[combination[0]] == board[combination[1]] && board[combination[1]] == board[combination[2]] {
+                return true
+            }
+        }
+        return false
+    }
+    
+    private func checkForTie() -> Bool {
+        for cell in board {
+            if cell == 0 {
+                return false
+            }
+        }
+        return true
+    }
+    private func disableBoard() {
+        for index in 0..<board.count {
+            board[index] = -1
+        }
+    }
+    
 }
-
---
+++ tic_tac_toeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tic_tac_toeApp.swift
+//  tic_tac_toe
+//
+//  Created by Ifenna Ekwenem on 5/10/23.
+//
+
+import SwiftUI
+
+@main
+struct tic_tac_toeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
